32f9f1950250718cb125b1988bcf36b072e35ebc
PhilippNiklas <philippd1502@gmail.com>
2021-03-06 17:34:43 +0100

    addded advanced serializer settings

--- backend/seating_plan_manager/serializers.py
+++ backend/seating_plan_manager/serializers.py
@@ -15,3 +15,40 @@ class SeatingPlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = SeatingPlan
         fields = '__all__'
+
+    def create(self, validated_data):
+        title = validated_data.pop('title')
+        new_plan = SeatingPlan.objects.create(title=title)
+        new_plan.save()
+
+        new_board = []
+        new_board_student = []
+        
+        for i in valdidated_data:
+            new_board.append(dict(i))
+        
+        for i in new_board:
+            new_student = Student(tablename=i['tablename'], firstname=i['firstname'], lastname=i['lastname'])
+            new_student.save()
+            new_board_student.append(new_student)
+        
+        new_plan.plan.set(new_board_student)
+
+        return new_plan
+
+    def update(self, instance, validated_data):
+        new_board = []
+
+        for i in validated_data['plan']:
+            new_board.append(dict(i))
+        
+        for i in new_board:
+            updated_student = instance.plan.filter(tablename=i['tablename']).update(firstname=i['firstname'], lastname['lastname'])
+
+            if updated_student==0:
+                new_student = Student(firstname=i['firstname'], lastname=i['lastname'], tablename=i['tablename'])
+                new_student.save()
+                instance.save()
+                instance.plan.add(new_student)
+        
+        return instance
