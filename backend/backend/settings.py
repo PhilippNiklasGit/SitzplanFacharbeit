79eceda4e356c40a5d61b89b1cf4ec040c83fb6c
PhilippNiklas <philippd1502@gmail.com>
2021-03-06 17:15:24 +0100

    changed location of initialised app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'seating_plan_manager',
     'corsheaders',
     'rest_framework',
     'knox',
+    
 ]
 
 REST_FRAMEWORK = {
