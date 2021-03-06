9d90eea25345be7665ff9d72ed3d798a561cc441
PhilippNiklas <philippd1502@gmail.com>
2021-03-06 17:59:50 +0100

    added account urls and added them in settings

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('seating_plan_manager.urls'))
+    path('', include('seating_plan_manager.urls')),
+    path('', include('accounts.urls'))
 ]
