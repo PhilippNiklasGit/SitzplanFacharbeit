3474b87d05ce2802c8d000383759f979b53e59da
PhilippNiklas <philippd1502@gmail.com>
2021-03-06 17:48:51 +0100

    added account manager and serializers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'seating_plan_manager',
+    'accounts',
     'corsheaders',
     'rest_framework',
     'knox',
